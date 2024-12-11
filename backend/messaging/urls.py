dcf02d1cbd58cca57267d5b15daf8c4366760689
Aryan <58602508+codernotme@users.noreply.github.com>
2024-12-11 22:17:50 +0530

    discard

--- backend/messaging/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import process_file
-from .views import send_messages
-
-urlpatterns = [
-    path('process-file/', process_file, name='process_file'),
-    path('send-messages/', send_messages, name='send_messages'),
-]
